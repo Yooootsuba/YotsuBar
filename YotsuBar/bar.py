fc10aea3222e2f4b6c717ea41317417483bfe83a
Yotsuba <happy819tw@gmail.com>
2020-06-09 05:55:15 +0800

    Context manager function

--- YotsuBar/bar.py
+++ YotsuBar/bar.py
@@ -55,3 +55,11 @@ class YotsuBar:
                             self.bar_prefix,
                             self.top_text.center(self.width),
                             self.bar_suffix))
+
+
+    def __enter__(self):
+        return self
+
+
+    def __exit__(self, type, value, traceback):
+        pass
