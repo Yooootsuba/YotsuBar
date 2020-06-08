71e1f83771f439e2b9e0ee5ab3b800b05f625faf
Yotsuba <happy819tw@gmail.com>
2020-06-09 05:35:29 +0800

    Update description in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
       name        = 'YotsuBar',
       version     = '1.0.0',
-      description = 'Python progress bar',
+      description = 'A progress bar module in Python',
       author      = 'Yotsuba',
       license     = 'MIT',
       packages    = find_packages(),
