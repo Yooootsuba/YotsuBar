810c35df6f5d9dcc737cd6dd334a63bf715189e0
Yotsuba <happy819tw@gmail.com>
2020-06-09 03:58:39 +0800

    Add bar.py, test.py and demo.gif

---
+++ YotsuBar/bar.py
@@ -0,0 +1,45 @@
+import sys
+
+
+class YotsuBar:
+
+    def __init__(self,
+                 max           = 100,
+                 width         = 50,
+                 top_text      = None,
+                 left_text     = '',
+                 right_text    = '',
+                 bar_prefix    = '[',
+                 bar_fill_char = '=',
+                 bar_suffix    = ']',
+                 hide_cursor   = False):
+
+        self.index         = 0
+        self.max           = max
+        self.width         = width
+        self.top_text      = top_text
+        self.left_text     = left_text
+        self.right_text    = right_text
+        self.bar_prefix    = bar_prefix
+        self.bar_fill_char = bar_fill_char
+        self.bar_suffix    = bar_suffix
+        self.hide_cursor   = hide_cursor
+
+
+    def next(self):
+        self.index += 1
+        self.write()
+
+
+    def write(self):
+        sys.stdout.write('\r%s%s%s%s %d%%%s' % (self.left_text,
+                                                self.bar_prefix,
+                                                (int(self.index / self.max * self.width) * self.bar_fill_char).ljust(self.width, ' '),
+                                                self.bar_suffix,
+                                                self.get_percent(),
+                                                self.right_text))
+        sys.stdout.flush()
+
+        
+    def get_percent(self):
+        return self.index / self.max * 100
---
+++ test.py
@@ -0,0 +1,21 @@
+import time
+from YotsuBar.bar import YotsuBar
+
+
+ASCII_ART = '''
+                                     _
+                   \_/ _ _|_  _     |_)  _. ._
+                    | (_) |_ _> |_| |_) (_| |
+
+            '''
+
+
+print(ASCII_ART)
+
+bar = YotsuBar(left_text = 'YotsuBar ')
+
+for i in range(100):
+    time.sleep(0.05)
+    bar.next()
+
+print('\n')
