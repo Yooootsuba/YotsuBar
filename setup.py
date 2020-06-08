161496470b990b7d01449b2ca9a550ad13dbe8e7
Yotsuba <happy819tw@gmail.com>
2020-06-08 19:46:06 +0800

    Init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+      name        = 'YotsuBar',
+      version     = '1.0.0',
+      description = 'Python progress bar',
+      author      = 'Yotsuba',
+      license     = 'MIT',
+      packages    = find_packages(),
+      )
